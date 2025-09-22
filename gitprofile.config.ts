b2633d33b4bb0142bbd3298375db1c604f4282b5
shuaix15 <157852091+shuaix15@users.noreply.github.com>
2025-09-21 22:12:19 -0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -173,10 +173,11 @@ const CONFIG = {
 
   // Optional Footer. Supports plain text or HTML.
   footer: `Made with <a 
-      class="text-primary" href=""
+      class="text-primary" href="https://github.com/arifszn/gitprofile"
       target="_blank"
-      rel="noreferrer",
-  >GitProfile</a>,
+      rel="noreferrer"
+    >GitProfile</a> and ❤️`,
+
   enablePWA: true,
 };
 
